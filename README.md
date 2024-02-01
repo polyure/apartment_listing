37ece96765c1782543d5d43eb6b765852326e861
Tommy Johansson <tommy.johansson@gmail.com>
2024-02-01 16:43:52 +0200

    Removed the need for .csv file, changed spreadsheet from .xlsm to .xlsx, updated README, and added .gitignore file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A python script that calls Oikotie API, saves found apartments on a map, and creates a .csv file to be used in addition to an .xlsm file.
+A python script that calls Oikotie API, saves found apartments on a map, and appends them to an .xlsx file. Note, xlwings requires Excel installation on the computer.
--- asunnot.py
+++ asunnot.py
@@ -2,7 +2,7 @@ from geopy.geocoders import Nominatim
 import folium
 import requests
 import pandas as pd
-import csv
+import xlwings as xw
 
 URL = "https://asunnot.oikotie.fi/vuokra-asunnot"
 API_URL = "https://asunnot.oikotie.fi/api/cards"
@@ -37,42 +37,50 @@ LOCATIONS = '[[14694,5,"00100, Helsinki"],\
             
 PARAMS = {'cardType':101, 'limit':1000,
           'locations':LOCATIONS,
-          'offset':0, 'constructionYear[max]':2023, 'roomCount[]':[2,3,4], 'price[min]':800, 'price[max]':1500, 'size[min]':55, 'size[max]':250, 'sortBy':"published_sort_desc"}
+          'offset':0, 'constructionYear[max]':2023, 'roomCount[]':[2,3,4], 'price[min]':800, 'price[max]':1500, 'size[min]':50, 'size[max]':250, 'sortBy':"published_sort_desc"} # cardType 101 for rentals, 100 for sale
 HEADERS = {"OTA-cuid":cuid, "OTA-loaded":loaded, "OTA-token":token}
 
 r = requests.get(url=API_URL, params=PARAMS, headers=HEADERS)
 data=r.json()
 fields = ["url","rooms","roomConfiguration","price","published","size","latitude","longitude","coordinates","buildingData"] # URL, huoneet, otsikko, hinta, päivämäärä, pinta-ala ja buildingData, jonka sisältö määritellään if-lauseessa
 datalist = []
-with open("asunnot.csv", "w", newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    for i in data['cards']:
-        row = []
-        price = 0
-        size = 0
-        for j in i:
-            if j in fields:
-                if (j == "buildingData"):
-                    row.append(i[j]['address'])
-                    row.append(i[j]['district'])
-                    row.append(i[j]['city'])
-                    row.append(i[j]['year'])
-                elif (j == "coordinates"):
-                    row.append(i[j]['latitude'])
-                    row.append(i[j]['longitude'])
-                else:
-                    row.append(i[j])
-        writer.writerow(row)
-        datalist.append(row)
-csvfile.close()
-
+for i in data['cards']:
+    row = []
+    price = 0
+    size = 0
+    for j in i:
+        if j in fields:
+            if (j == "buildingData"):
+                row.append(i[j]['address'])
+                row.append(i[j]['district'])
+                row.append(i[j]['city'])
+                row.append(i[j]['year'])
+            elif (j == "coordinates"):
+                row.append(i[j]['latitude'])
+                row.append(i[j]['longitude'])
+            else:
+                row.append(i[j])
+    datalist.append(row)
+        
 df = pd.DataFrame(datalist, columns = ['url', 'rooms', 'roomConfiguration', 'price', 'published', 'size', 'address', 'district', 'city', 'buildYear', 'latitude', 'longitude'])
+
+wb = xw.Book('asunnot3.xlsx')
+sheet = wb.sheets['CSV']
+
+sheet.clear_contents()
+sheet['A1'].options(index=False, header=False).value = df
+
+wb.save("asunnot3.xlsx")
+wb.close()
+
+
 df['price'] = df['price'].replace(to_replace = "[^0-9]", value = "", regex = True)
 df['price'] = df['price'].apply(pd.to_numeric)
 df['perSquareMetre'] = df['price']/df['size']
 
 df['quintile'] = pd.qcut(df['perSquareMetre'], 5, labels=False)
 mean_rent = df['price'].mean() # For later implementations
+
 
 app = Nominatim(user_agent='tommy')
 apartment_map = folium.Map([60.1868, 24.933], zoom_start=12) # Center the map on Helsinki
